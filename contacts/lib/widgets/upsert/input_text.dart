13f5b594e3be036e2f772e8a6502d7fdd0b4d3ca
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-19 23:19:45 +0430

    create input widget

---
+++ contacts/lib/widgets/upsert/input_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class InputOutline extends StatelessWidget {
+  final String hint;
+  final String lable;
+  final TextInputType inputTtype;
+  final TextEditingController controller;
+
+  const InputOutline(
+      {super.key,
+      required this.hint,
+      required this.controller,
+      required this.lable,
+      this.inputTtype = TextInputType.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: TextField(
+            controller: controller,
+            keyboardType: inputTtype,
+            decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                hintText: hint,
+                labelText: lable)));
+  }
+}
