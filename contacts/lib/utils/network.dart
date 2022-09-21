d186c4517491cbd19e3035cb1a1b370dbaa114e0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-21 13:57:42 +0430

    change network response for actions

--- contacts/lib/utils/network.dart
+++ contacts/lib/utils/network.dart
@@ -9,48 +9,71 @@ class Network {
   //? Contacts List
   static List<Contact> contacts = [];
 
-//! Get Data From Server
-  static void getData() async {
-    http.get(Network.uri).then((response) {
-      if (response.statusCode == 200) {
-        contacts.clear();
-        List jsonDecode = json.decode(response.body);
-        for (var jobj in jsonDecode) {
-          contacts.add(Contact.fromJson(jobj));
+  //! Get Data From Server
+  static Future<bool> getData() async {
+    try {
+      var request = await http.get(Network.uri);
+      if (request.statusCode == 200) {
+        String response = request.body;
+        List jsonDecode = json.decode(response);
+        for (var jObject in jsonDecode) {
+          contacts.add(jObject);
         }
+        return true;
       }
-    });
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 
   //* Post Data For Create New Contact
-  static void postData(
+  static Future<bool> postData(
       {required String mobileNo, required String fullName}) async {
     Contact contact = Contact(fullName: fullName, mobileNo: mobileNo);
-    http.post(Network.uri, body: contact.toJson()).then((response) {
-      if (response.statusCode == 200) {}
-    });
+    // http.post(Network.uri, body: contact.toJson()).then((response) {
+    //   if (response.statusCode == 200) {}
+    // });
+    try {
+      var reqeuest = await http.post(Network.uri, body: contact.toJson());
+      if (reqeuest.statusCode == 200) {
+        getData();
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 
   //* Pust Data For Edit Exist Contact
-  static void pustData(
+  static Future<bool> pustData(
       {required String mobileNo,
       required String fullName,
       required int id}) async {
     Contact contact = Contact(fullName: fullName, mobileNo: mobileNo);
     Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts/$id');
 
-    http.put(Network.uri, body: contact.toJson()).then((response) {
-      if (response.statusCode == 200) {}
-    });
+    try {
+      var request = await http.put(Network.uri, body: contact.toJson());
+      if (request.statusCode == 200) {
+        getData();
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 
   //! Delete Customer
-  static void deleteData(int id) async {
+  static Future<bool> deleteData(int id) async {
     Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts/$id');
-    http.delete(Network.uri).then((response) {
-      if (response.statusCode == 200) {
-        getData();
-      }
-    });
+    try {
+      var request = await http.delete(Network.uri);
+      return request.statusCode == 200;
+    } catch (e) {
+      return false;
+    }
   }
 }
