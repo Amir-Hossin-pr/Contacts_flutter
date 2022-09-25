84db2c0d2cc52472f07c089d3ee0e95942e0abf4
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-25 22:51:23 +0330

    update controller actions

--- contacts/lib/controllers/contacts_controller.dart
+++ contacts/lib/controllers/contacts_controller.dart
@@ -1,3 +1,23 @@
+import 'package:contacts/models/contact.dart';
 import 'package:get/state_manager.dart';
 
-class ContactsController extends GetxController {}
+class ContactsController extends GetxController {
+  List<Contact> contacts = <Contact>[];
+  Rx<Contact> currentContact = Contact(fullName: '', mobileNo: '', id: 0).obs;
+
+  Future<bool> insertContact(String fullName, String mobileNo) async {
+    return true;
+  }
+
+  Future<bool> updateContact(int id, String fullName, String mobileNo) async {
+    return true;
+  }
+
+  Future<bool> deleteContact(int id) async {
+    return true;
+  }
+
+  Future<List<Contact>> getContacts() async {
+    return [];
+  }
+}
