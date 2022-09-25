d8f4f0f329d54bb5469f7ed30ea6c16c6e5e5513
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-25 23:20:12 +0330

    Update network.dart

--- contacts/lib/utils/network.dart
+++ contacts/lib/utils/network.dart
@@ -1,79 +1,103 @@
-import 'dart:convert';
-
-import 'package:contacts/models/contact.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class Network {
-  static Uri uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts');
+  final String baseURL;
 
-  //? Contacts List
-  static List<Contact> contacts = [];
+  Network({required this.baseURL});
 
-  //! Get Data From Server
-  static Future<bool> getData() async {
+  // static Uri uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts');
+
+  Future<Response> callApi(
+      String url, ApiCallType callType, Object? data) async {
     try {
-      Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts');
-      var request = await http.get(Network.uri);
-      if (request.statusCode == 200) {
-        contacts.clear();
-        String response = request.body;
-        List jsonDecode = json.decode(response);
-        for (var jObject in jsonDecode) {
-          contacts.add(Contact.fromJson(jObject));
-        }
-        return true;
+      var uri = Uri.parse(baseURL + url);
+      switch (callType) {
+        case ApiCallType.post:
+          return await post(uri);
+        case ApiCallType.put:
+          return await put(uri);
+        case ApiCallType.delete:
+          return await delete(uri);
+        case ApiCallType.get:
+          return await get(uri);
+        default:
+          return Response('No Action', 404);
       }
-      return false;
     } catch (e) {
-      return false;
+      return Response('Exception', 500);
     }
   }
 
-  //* Post Data For Create New Contact
-  static Future<bool> postData(
-      {required String mobileNo, required String fullName}) async {
-    Contact contact = Contact(fullName: fullName, mobileNo: mobileNo, id: 0);
+  // //? Contacts List
+  // static List<Contact> contacts = [];
 
-    try {
-      var request = await http.post(Network.uri, body: contact.toJson());
-      if (request.statusCode == 200 || request.statusCode == 201) {
-        getData();
-        return true;
-      }
-      return false;
-    } catch (e) {
-      return false;
-    }
-  }
+  // //! Get Data From Server
+  // static Future<bool> getData() async {
+  //   try {
+  //     Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts');
+  //     var request = await http.get(Network.uri);
+  //     if (request.statusCode == 200) {
+  //       contacts.clear();
+  //       String response = request.body;
+  //       List jsonDecode = json.decode(response);
+  //       for (var jObject in jsonDecode) {
+  //         contacts.add(Contact.fromJson(jObject));
+  //       }
+  //       return true;
+  //     }
+  //     return false;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
 
-  //* Pust Data For Edit Exist Contact
-  static Future<bool> pustData(
-      {required String mobileNo,
-      required String fullName,
-      required int id}) async {
-    Contact contact = Contact(fullName: fullName, mobileNo: mobileNo, id: id);
-    Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts/$id');
+  // //* Post Data For Create New Contact
+  // static Future<bool> postData(
+  //     {required String mobileNo, required String fullName}) async {
+  //   Contact contact = Contact(fullName: fullName, mobileNo: mobileNo, id: 0);
 
-    try {
-      var request = await http.put(Network.uri, body: contact.toJson());
-      if (request.statusCode == 200 || request.statusCode == 201) {
-        getData();
-        return true;
-      }
-      return false;
-    } catch (e) {
-      return false;
-    }
-  }
+  //   try {
+  //     var request = await http.post(Network.uri, body: contact.toJson());
+  //     if (request.statusCode == 200 || request.statusCode == 201) {
+  //       getData();
+  //       return true;
+  //     }
+  //     return false;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
 
-  //! Delete Customer
-  static Future<bool> deleteData(int id) async {
-    Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts/$id');
-    try {
-      var request = await http.delete(Network.uri);
-      return request.statusCode == 200;
-    } catch (e) {
-      return false;
-    }
-  }
+  // //* Pust Data For Edit Exist Contact
+  // static Future<bool> pustData(
+  //     {required String mobileNo,
+  //     required String fullName,
+  //     required int id}) async {
+  //   Contact contact = Contact(fullName: fullName, mobileNo: mobileNo, id: id);
+  //   Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts/$id');
+
+  //   try {
+  //     var request = await http.put(Network.uri, body: contact.toJson());
+  //     if (request.statusCode == 200 || request.statusCode == 201) {
+  //       getData();
+  //       return true;
+  //     }
+  //     return false;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
+
+  // //! Delete Customer
+  // static Future<bool> deleteData(int id) async {
+  //   Network.uri = Uri.parse('https://retoolapi.dev/qgQGp3/contacts/$id');
+  //   try {
+  //     var request = await http.delete(Network.uri);
+  //     return request.statusCode == 200;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
 }
+
+enum ApiCallType { post, get, put, delete }
