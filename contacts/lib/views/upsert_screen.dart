57adac1a23dabe45f0d0837f60405a03533f2552
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-25 23:55:46 +0330

    use getx controller for user

--- contacts/lib/views/upsert_screen.dart
+++ contacts/lib/views/upsert_screen.dart
@@ -1,115 +1,108 @@
-import 'package:contacts/utils/network.dart';
+import 'package:contacts/controllers/contacts_controller.dart';
+import 'package:contacts/models/contact.dart';
 import 'package:contacts/widgets/upsert/input_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
 
-class UpsertScreen extends StatefulWidget {
-  const UpsertScreen({super.key});
+class UpsertScreen extends StatelessWidget {
+  final TextEditingController fullName = TextEditingController();
+  final TextEditingController mobileNo = TextEditingController();
+  int contactId = -1;
 
-  static TextEditingController fullName = TextEditingController();
-  static TextEditingController mobileNo = TextEditingController();
-  static int contactId = -1;
+  final formKey = GlobalKey<FormState>();
 
-  @override
-  State<UpsertScreen> createState() => _UpsertScreenState();
-}
-
-class _UpsertScreenState extends State<UpsertScreen> {
   @override
   Widget build(BuildContext context) {
-    String title = UpsertScreen.contactId == -1
-        ? "Add New Contact"
-        : 'Edit `${UpsertScreen.fullName.text}`';
-
-    final formKey = GlobalKey<FormState>();
-
-    return Form(
-        key: formKey,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-            leading: IconButton(
-              onPressed: () {
-                UpsertScreen.contactId = -1;
-                UpsertScreen.fullName.text = "";
-                UpsertScreen.mobileNo.text = "";
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.arrow_back),
+    return GetBuilder(builder: (ContactsController contactsController) {
+      if (contactsController.currentContact.value.id != -1) {
+        contactId = contactsController.currentContact.value.id;
+        fullName.text = contactsController.currentContact.value.fullName;
+        mobileNo.text = contactsController.currentContact.value.mobileNo;
+      }
+      return Form(
+          key: formKey,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(contactId == -1
+                  ? 'Create New Contact'
+                  : 'Update ${fullName.text}'),
+              leading: IconButton(
+                onPressed: () {
+                  contactsController.currentContact.value =
+                      Contact(fullName: '', mobileNo: '', id: -1);
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back),
+              ),
+              backgroundColor: contactId == -1 ? Colors.blue : Colors.amber,
             ),
-            backgroundColor:
-                UpsertScreen.contactId == -1 ? Colors.blue : Colors.amber,
-          ),
-          body: Column(
-            children: [
-              InputOutline(
-                  hint: 'نام و نام خانوادگی',
-                  lable: 'نام و نام خانوادگی',
-                  errorText: 'لطفا نام را وارد کنید',
-                  controller: UpsertScreen.fullName),
-              InputOutline(
-                  hint: 'موبایل',
-                  lable: 'شماره موبایل',
-                  errorText: 'لطفا شماره موبایل را وارد کنید',
-                  inputTtype: TextInputType.phone,
-                  controller: UpsertScreen.mobileNo),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                      padding:
-                          MaterialStateProperty.all(const EdgeInsets.all(15))),
-                  onPressed: () async {
-                    void clearForm() {
-                      UpsertScreen.contactId = -1;
-                      UpsertScreen.fullName.text = "";
-                      UpsertScreen.mobileNo.text = "";
-                    }
+            body: Column(
+              children: [
+                InputOutline(
+                    hint: 'نام و نام خانوادگی',
+                    lable: 'نام و نام خانوادگی',
+                    errorText: 'لطفا نام را وارد کنید',
+                    controller: fullName),
+                InputOutline(
+                    hint: 'موبایل',
+                    lable: 'شماره موبایل',
+                    errorText: 'لطفا شماره موبایل را وارد کنید',
+                    inputTtype: TextInputType.phone,
+                    controller: mobileNo),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.all(15))),
+                    onPressed: () async {
+                      void clearForm() {
+                        contactsController.currentContact.value =
+                            Contact(fullName: '', mobileNo: '', id: -1);
+                      }
 
-                    void showSnackbar(String message, bool success) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(
-                        message,
-                        style: TextStyle(
-                            color: success ? Colors.green : Colors.red),
-                      )));
-                    }
+                      void showSnackbar(String message, bool success) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                          message,
+                          style: TextStyle(
+                              color: success ? Colors.green : Colors.red),
+                        )));
+                      }
 
-                    if (formKey.currentState!.validate()) {
-                      if (UpsertScreen.contactId == -1) {
-                        if (await Network.postData(
-                            mobileNo: UpsertScreen.mobileNo.text,
-                            fullName: UpsertScreen.fullName.text)) {
-                          showSnackbar(
-                              '`${UpsertScreen.fullName.text}` Inserted Successfuly',
-                              true);
-                          clearForm();
-                          Navigator.pop(context);
-                        }
-                      } else {
-                        if (await Network.pustData(
-                            mobileNo: UpsertScreen.mobileNo.text,
-                            fullName: UpsertScreen.fullName.text,
-                            id: UpsertScreen.contactId)) {
-                          showSnackbar(
-                              '`${UpsertScreen.fullName.text}` Updated Successfuly',
-                              true);
-                          clearForm();
-                          Navigator.pop(context);
+                      if (formKey.currentState!.validate()) {
+                        if (contactId == -1) {
+                          if (await contactsController.insertContact(
+                              fullName.text, mobileNo.text)) {
+                            showSnackbar(
+                                '`${fullName.text}` Inserted Successfuly',
+                                true);
+                            clearForm();
+                            Navigator.pop(context);
+                          }
+                        } else {
+                          if (await contactsController.updateContact(
+                              contactId, fullName.text, mobileNo.text)) {
+                            showSnackbar(
+                                '`${fullName.text}` Updated Successfuly', true);
+                            clearForm();
+                            Navigator.pop(context);
+                          }
                         }
                       }
-                    }
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('ثبت مخاطب'),
-                      Icon(Icons.save),
-                    ],
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text('ثبت مخاطب'),
+                        Icon(Icons.save),
+                      ],
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
-        ));
+                )
+              ],
+            ),
+          ));
+    });
   }
 }
