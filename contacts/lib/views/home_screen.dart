4ab2cba28641ad180737aae4b51d1fa291e11600
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-25 23:55:31 +0330

    change getx to obx object

--- contacts/lib/views/home_screen.dart
+++ contacts/lib/views/home_screen.dart
@@ -1,26 +1,12 @@
+import 'package:contacts/controllers/contacts_controller.dart';
 import 'package:contacts/models/contact.dart';
-import 'package:contacts/views/upsert_screen.dart';
-import 'package:contacts/utils/network.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Network.getData().then((value) {
-      if (value) {
-        setState(() {});
-      }
-    });
-  }
+  final contactsController = Get.find<ContactsController>();
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: [
           IconButton(
               onPressed: () async {
-                if (await Network.getData()) {
-                  setState(() {});
-                }
+                await contactsController.getContacts();
               },
               icon: const Icon(Icons.refresh))
         ],
@@ -41,107 +25,106 @@ class _HomeScreenState extends State<HomeScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.toNamed('/upsert')?.then((value) async {
-            await Network.getData();
-            setState(() {});
+            await contactsController.getContacts();
           });
         },
         child: const Icon(Icons.add),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text('${index + 1}'),
-            ),
-            trailing: IconButton(
-              onPressed: () {
-                Contact contact = Network.contacts[index];
-                UpsertScreen.fullName.text = contact.fullName;
-                UpsertScreen.mobileNo.text = contact.mobileNo;
-                UpsertScreen.contactId = contact.id;
-                Get.toNamed('/upsert')?.then((value) async {
-                  await Network.getData();
-                  setState(() {});
-                });
-              },
-              icon: const Icon(Icons.edit),
-            ),
-            title: Text(Network.contacts[index].fullName),
-            subtitle: Text(Network.contacts[index].mobileNo),
-            onTap: () {
-              var contact = Network.contacts[index];
-              showDialog(
-                  context: context,
-                  builder: (builder) => AlertDialog(
-                        actionsAlignment: MainAxisAlignment.center,
-                        icon: const Icon(Icons.delete),
-                        title: const Text('Delete Contact'),
-                        content: Text(
-                            'Are you sure to delete `${contact.fullName}` ?'),
-                        actions: [
-                          Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: ElevatedButton(
-                              onPressed: () async {
-                                var contact = Network.contacts[index];
-                                if (await Network.deleteData(contact.id)) {
-                                  Navigator.pop(context);
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                          content: Text(
-                                              'User ${contact.fullName} Deleted Successfuly',
-                                              style: const TextStyle(
-                                                  color: Colors.green))));
-                                } else {
-                                  ScaffoldMessenger.of(context)
-                                      .showSnackBar(SnackBar(
-                                          content: Text(
-                                    'User ${contact.fullName} Not Deleted',
-                                    style: const TextStyle(color: Colors.red),
-                                  )));
-                                }
-                                await Network.getData();
-                                setState(() {});
-                              },
-                              style: ButtonStyle(
-                                  padding: MaterialStateProperty.all(
-                                      const EdgeInsets.all(10)),
-                                  backgroundColor:
-                                      MaterialStateProperty.all(Colors.red)),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text('Delete'),
-                                  Icon(Icons.delete)
-                                ],
+      body: Obx(() => ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Text('${index + 1}'),
+                ),
+                trailing: IconButton(
+                  onPressed: () {
+                    Contact contact = contactsController.contacts[index];
+                    contactsController.currentContact.value = contact;
+                    Get.toNamed('/upsert')?.then((value) async {
+                      await contactsController.getContacts();
+                    });
+                  },
+                  icon: const Icon(Icons.edit),
+                ),
+                title: Text(contactsController.contacts[index].fullName),
+                subtitle: Text(contactsController.contacts[index].mobileNo),
+                onTap: () {
+                  var contact = contactsController.contacts[index];
+                  showDialog(
+                      context: context,
+                      builder: (builder) => AlertDialog(
+                            actionsAlignment: MainAxisAlignment.center,
+                            icon: const Icon(Icons.delete),
+                            title: const Text('Delete Contact'),
+                            content: Text(
+                                'Are you sure to delete `${contact.fullName}` ?'),
+                            actions: [
+                              Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: ElevatedButton(
+                                  onPressed: () async {
+                                    var contact =
+                                        contactsController.contacts[index];
+                                    if (await contactsController
+                                        .deleteContact(contact.id)) {
+                                      Navigator.pop(context);
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(
+                                                  'User ${contact.fullName} Deleted Successfuly',
+                                                  style: const TextStyle(
+                                                      color: Colors.green))));
+                                    } else {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(
+                                        'User ${contact.fullName} Not Deleted',
+                                        style:
+                                            const TextStyle(color: Colors.red),
+                                      )));
+                                    }
+                                    await contactsController.getContacts();
+                                  },
+                                  style: ButtonStyle(
+                                      padding: MaterialStateProperty.all(
+                                          const EdgeInsets.all(10)),
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              Colors.red)),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Text('Delete'),
+                                      Icon(Icons.delete)
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              style: ButtonStyle(
-                                  padding: MaterialStateProperty.all(
-                                      const EdgeInsets.all(10))),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text('Cancle'),
-                                  Icon(Icons.cancel)
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
-                      ));
+                              Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  style: ButtonStyle(
+                                      padding: MaterialStateProperty.all(
+                                          const EdgeInsets.all(10))),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Text('Cancle'),
+                                      Icon(Icons.cancel)
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ));
+                },
+              );
             },
-          );
-        },
-        itemCount: Network.contacts.length,
-      ),
+            itemCount: contactsController.contacts.length,
+          )),
     );
   }
 }
