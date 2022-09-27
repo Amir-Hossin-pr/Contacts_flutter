37528b78b1652edb21018e0819b1e344531dabb4
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-27 21:50:56 +0330

    create translate objects

--- contacts/lib/translate/translate.dart
+++ contacts/lib/translate/translate.dart
@@ -6,12 +6,26 @@ class Translate extends Translations {
         'en': {
           'fullName': 'Full Name',
           'mobileNo': 'Mobile Number',
-          'save': 'Save'
+          'save': 'Save',
+          'ChangeLang': 'Language Change',
+          'ChangeLangMessage': 'Language Changed Successfuly',
+          'mobileRequired': 'Mobile Number is required',
+          'fullNameRequired': 'Full Name is required',
+          'title': 'Contacts',
+          'createNew': 'Create New Contact',
+          'changeLang': 'Change Language'
         },
         'fa': {
           'fullName': 'نام و نام خانوادگی',
           'mobileNo': 'شماره موبایل',
-          'save': 'دخیره'
+          'save': 'دخیره',
+          'ChangeLang': 'تغییر زبان',
+          'ChangeLangMessage': 'زبان با موفقیت تغییر کرد',
+          'mobileRequired': 'شماره موبایل الزامی می باشد',
+          'fullNameRequired': 'نام و نام خانوادگی الزامی می باشد',
+          'title': 'لیست مخاطبان',
+          'createNew': 'افزودن مخاطب جدید',
+          'changeLang': 'تغییر زبان'
         }
       };
 }
