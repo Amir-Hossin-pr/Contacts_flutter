80bd5be46bfe17b7655273b41b399d173e163610
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-27 21:51:23 +0330

    set default localization

--- contacts/lib/app.dart
+++ contacts/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:contacts/bindings/home_binding.dart';
+import 'package:contacts/translate/translate.dart';
 import 'package:contacts/views/home_screen.dart';
 import 'package:contacts/views/upsert_screen.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      translations: Translate(),
+      locale: Locale('en'),
       getPages: [
         GetPage(
             name: '/home', page: () => HomeScreen(), binding: HomeBinding()),
